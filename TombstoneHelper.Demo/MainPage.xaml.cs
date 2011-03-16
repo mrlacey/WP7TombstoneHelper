9467f6629ae8ec26e164661df6298802058925e0
Matt Lacey <github@mrlacey.co.uk>
2011-03-16 22:31:39 +0000

    more demos and fix for slider

--- TombstoneHelper.Demo/MainPage.xaml.cs
+++ TombstoneHelper.Demo/MainPage.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
 namespace TombstoneHelper.Demo
@@ -20,6 +12,20 @@ namespace TombstoneHelper.Demo
             InitializeComponent();
         }
 
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            this.SaveState(typeof(ScrollViewer));
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            this.RestoreState();
+        }
+
         private void TextBoxes_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/pages/TextBoxes.xaml", UriKind.Relative));
@@ -28,6 +34,46 @@ namespace TombstoneHelper.Demo
         private void ListBoxes_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/pages/ListBoxes.xaml", UriKind.Relative));
+        }
+
+        private void Assorted_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/Assorted.xaml", UriKind.Relative));
+        }
+
+        private void Some_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/Some.xaml", UriKind.Relative));
+        }
+
+        private void PasswordBoxes_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/PasswordBoxes.xaml", UriKind.Relative));
+        }
+
+        private void CheckBoxes_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/CheckBoxes.xaml", UriKind.Relative));
+        }
+
+        private void RadioButtons_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/RadioButtons.xaml", UriKind.Relative));
+        }
+
+        private void ScrollViewers_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/ScrollViewerExample.xaml", UriKind.Relative));
+        }
+
+        private void Sliders_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/Sliders.xaml", UriKind.Relative));
+        }
+
+        private void AutoTombstonePage_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/pages/AutoTombstone.xaml", UriKind.Relative));
         }
     }
 }
---
+++ TombstoneHelper.Demo/pages/AutoTombstone.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TombstoneHelper.Demo.pages
+{
+    public partial class AutoTombstone : AutoTombstonePage
+    {
+        public AutoTombstone()
+        {
+            InitializeComponent();
+        }
+
+        // No need to do anything else here.
+        // We get tombstoning support because we inherit from 'AutoTombstonePage'
+    }
+}
