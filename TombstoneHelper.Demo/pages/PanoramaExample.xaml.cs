23e03d76a05ea28d4398de9fe18558ee807bf512
mrlacey <mrlacey@localhost>
2011-05-16 21:28:23 +0100

    Added extensibility support + added extensibility example for Panorama + some bug fixes

---
+++ TombstoneHelper.Demo/pages/PanoramaExample.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace TombstoneHelper.Demo.pages
+{
+    public partial class PanoramaExample : PhoneApplicationPage
+    {
+        public PanoramaExample()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatingFrom(System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+
+            TombstoneHelperExtensibility.RegisterCustomTombstoner(typeof(Panorama), new PanoramaTombstoner());
+            this.SaveState(e, 1, typeof(Panorama));
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            TombstoneHelperExtensibility.RegisterCustomTombstoner(typeof(Panorama), new PanoramaTombstoner());
+            this.RestoreState();
+        }
+    }
+}
---
+++ TombstoneHelper/PanoramaTombstoner.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace TombstoneHelper
+{
+    public class PanoramaTombstoner : ICanTombstone
+    {
+        public void Save(FrameworkElement element, int pivotItemIndex, PhoneApplicationPage toSaveFrom)
+        {
+            if (element is Panorama)
+            {
+                var p = element as Panorama;
+
+                if (!string.IsNullOrEmpty(p.Name) && (p.SelectedIndex > 0))
+                {
+                    toSaveFrom.SaveStateWithTombstoneHelper(p, pivotItemIndex, p.SelectedIndex, this);
+                }
+            }
+        }
+
+        public void Restore(FrameworkElement toRestoreTo, object details)
+        {
+            if (toRestoreTo is Panorama)
+            {
+               // (toRestoreTo as Panorama).Loaded += (o, args) => { (toRestoreTo as Panorama).DefaultItem = int.Parse(details.ToString()); };
+                (toRestoreTo as Panorama).DefaultItem = (toRestoreTo as Panorama).Items[int.Parse(details.ToString())]; 
+            }
+        }
+    }
+}
